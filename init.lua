17ec19c6ee7120b275b36b2be716cc4b107d78e3
Andrew Velasquez <andrewvel35@gmail.com>
2025-04-07 02:38:08 -0400

    Made changes to init.lua to stop giving me warnings

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 require("andrew.core")
 require("andrew.lazy")
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.python3_host_prog = vim.fn.expand("~/.pyenv/versions/neovim-py/bin/python3")
